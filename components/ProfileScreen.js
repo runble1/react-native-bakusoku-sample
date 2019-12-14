32a88a1a745feefce3960b5511152ace87f75ca2
0ky3r43m78m070b@gmail.com <0ky3r43m78m090b@gmail.com>
2019-12-14 17:35:51 +0900

    Add reset button

--- components/ProfileScreen.js
+++ components/ProfileScreen.js
@@ -1,19 +1,62 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, AsyncStorage, Alert } from "react-native";
 import { Button } from "react-native-elements";
+import { connect } from "react-redux";
+
+import * as actions from "../actions";
 
 class ProfileScreen extends React.Component {
+  onResetButtonPress = async key => {
+    // 'key'に対応するAsyncStorageの中身をリセット(非同期処理)
+    await AsyncStorage.removeItem(key);
+
+    if (key === "allReviews") {
+      this.props.fetchAllReviews();
+    }
+
+    Alert.alert(
+      "Reset",
+      `'${key}' in AsyncStorage has been removed.`,
+      [{ text: "OK" }],
+      { cancelable: false }
+    );
+  };
+
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center" }}>
-        <Text>This is ProfileScreen</Text>
-        <Button // ←追記部分
-          title="Go to Setting1Screen"
-          onPress={() => this.props.navigation.navigate("setting1")}
-        />
+        <View style={{ padding: 20 }}>
+          <Button
+            title="Go to Setting1Screen"
+            onPress={() => this.props.navigation.navigate("setting1")}
+          />
+        </View>
+
+        <View style={{ padding: 20 }}>
+          <Button // ←追記部分
+            title="Reset welcome page"
+            buttonStyle={{ backgroundColor: "red" }}
+            onPress={() => this.onResetButtonPress("isInitialized")}
+          />
+        </View>
+
+        <View style={{ padding: 20 }}>
+          <Button // ←追記部分
+            title="Reset all review data"
+            buttonStyle={{ backgroundColor: "red" }}
+            onPress={() => this.onResetButtonPress("allReviews")}
+          />
+        </View>
       </View>
     );
   }
 }
 
-export default ProfileScreen;
+const mapStateToProps = state => {
+  // ←追記部分
+  return {
+    allReviews: state.review.allReviews
+  };
+};
+
+export default connect(mapStateToProps, actions)(ProfileScreen);
